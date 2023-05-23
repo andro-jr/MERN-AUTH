42bd2c7b4d64538cc56692739f53bd521fe1e05d
prabin <prabin20panta@gmail.com>
2023-05-23 20:31:03 +0545

    backend setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import cookieParser from 'cookie-parser';
 const PORT = process.env.PORT || 5000;
 const app = express();
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import userRoutes from './routes/userRoutes.js';
+import connectDB from './config/db.js';
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello from server');
 });
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
